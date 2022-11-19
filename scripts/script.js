50f8266ee1d8f88454ad4227ff78fe2547907a22
Zilmar Junio Andrade Vieira <zilmarjjunio@gmail.com>
2022-11-19 20:13:50 -0300

    novos

---
+++ scripts/script.js
@@ -0,0 +1,21 @@
+function Color1() {
+    document.body.style.backgroundColor = "rgb(0, 0, 0)";
+}
+function Color2() {
+    document.body.style.backgroundColor = "rgb(139, 0, 0)";
+}
+function Color3() {
+    document.body.style.backgroundColor = "rgba(47, 79, 79, 1)";
+}
+function Color4() {
+    document.body.style.backgroundColor = "rgb(105, 105, 105)";
+}
+function Color5() {
+    document.body.style.backgroundColor = "rgb(128, 128, 128)";
+}
+function Color6() {
+    document.body.style.backgroundColor = "rgb(168, 168, 168)";
+}
+function ColorReset() {
+    document.body.style.backgroundColor = "rgba(32, 33, 36, 0.9)"
+}
